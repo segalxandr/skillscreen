9f0e8348a8e75d0e5aad08a1edf1f0b1ccecb08a
alexsegal <asegal2012@gmail.com>
2018-05-10 21:10:15 -0400

    updated starship to responsive css

--- src/app/starship/starship.component.ts
+++ src/app/starship/starship.component.ts
@@ -9,26 +9,39 @@ import { FormBuilder } from '@angular/forms';
 })
 export class StarshipComponent implements OnInit {
   state = {};
-  form: any;
-  // private starshipComponents = [];
   starshipComponentsSelected = [];
-  starship = new Starship();
+  starship: Starship;
   constructor(private router: Router) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.starship = new Starship();
+  }
 
   checkChange(e: any) {
     console.log(e.target.toString());
     const obj = this.starship.components.find(x => x.type === e.target['id']);
     obj.selected = e.target.checked;
-    if (e.target.checked) {
-      this.starshipComponentsSelected.push(obj);
-    } else {
-      const indx = this.starshipComponentsSelected.indexOf(obj);
-      this.starshipComponentsSelected.splice(indx);
-    }
+    this.updateSelected();
+
   }
   goBack() {
     this.router.navigate(['/']);
+  }
+  selectAllComponents() {
+    this.starship.components.forEach(x => {
+      x.selected = true;
+    });
+    this.updateSelected();
+  }
+  reset() {
+    this.starship.components.forEach(x => {
+      x.selected = false;
+    });
+    this.updateSelected();
+  }
+  updateSelected() {
+    this.starshipComponentsSelected = this.starship.components.filter(
+      x => x.selected
+    );
   }
 }
--- src/app/starship/starship.ts
+++ src/app/starship/starship.ts
@@ -15,11 +15,11 @@ export class Starship {
     this.components = [
       { name: 'Engine', type: 'engine', selected: false, id: 1, src: '../../assets/starship_engines00.svg' },
       { name: 'Tractor Beam', type: 'tractorbeam', selected: false, id: 2, src: '../../assets/starship_tractorbeam00.svg' },
-      { name: 'Sensors', type: 'sensors', selected: false, id: 3, src: '../../assets/starship_sensors00.svg' },
+      { name: 'Escape Pods', type: 'escapepods', selected: false, id: 7, src: '../../assets/starship_escapepods00.svg' },
       { name: 'Shields', type: 'shields', selected: false, id: 4, src: '../../assets/starship_shields00.svg' },
       { name: 'Navi Computer', type: 'navicomputer', selected: false, id: 5, src: '../../assets/starship_navicomputer00.svg' },
-      { name: 'Comms', type: 'comms', selected: false, id: 6, src: '../../assets/starship_comms00.svg' },
-      { name: 'Escape Pods', type: 'escapepods', selected: false, id: 7, src: '../../assets/starship_escapepods00.svg' }
+      { name: 'Sensors', type: 'sensors', selected: false, id: 3, src: '../../assets/starship_sensors00.svg' },
+      { name: 'Comms', type: 'comms', selected: false, id: 6, src: '../../assets/starship_comms00.svg' }
     ];
 
   }
